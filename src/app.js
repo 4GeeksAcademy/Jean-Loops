6141382fc5c1ccef2635abae9726c7c0b8c57326
JeankenderJoseph3066 <jeankenderjoseph28@gmail.com>
2025-02-25 00:53:18 +0000

    Done

--- src/app.js
+++ src/app.js
@@ -4,21 +4,28 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+// factored
+// updates variable names
+// updates for loops
+// moved functionality  into function then called in window.onload
+// add comments to better describe my code
+// add more items to myb array to increase varity
+window.onload = function () {
   //write your code here
-  let pronoun = ['the', 'our', 'his', 'hers'];
-  let adj = ['great', 'big', 'little', 'small'];
-  let noun = ['jogger', 'racoon', 'democrat', 'republican' ];
-  let domain = ['.com', '.net', '.gov', '.org'];
-
-  for(let i = 0; i < pronoun.length; i++) {
-    for(let a = 0; a < adj.length; a++) {
-      for(let b = 0; b <noun.length; b++) {
-        for(let c = 0; c <domain.length; c++) {
-       console.log(pronoun[i] + adj[a] + noun[b] + domain[c]) }
+  // create the "parts" of the band names
+  let pronouns = ['the', 'our', 'his', 'hers'];
+  let adjs = ['great', 'big', 'little', 'small'];
+  let nouns = ['jogger', 'racoon', 'democrat', 'republican'];
+  let domains = ['.com', '.net', '.gov', '.org'];
+// loop through  parts to create band name
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let domain of domains) {
+        console.log(pronoun+adj+noun+domain)
+          }
+        }
       }
     }
-  }
-  console.log("Hello Rigo from the console!");
-};
+    console.log("Hello Rigo from the console!");
+  };
